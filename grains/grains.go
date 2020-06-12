f806934e780ddd182e53e841608b426a5e847dd6
Dave McAlpine <dave@mcalpine.io>
2020-06-11 21:03:30 -0400

    initial grains commit

---
+++ grains/grains.go
@@ -0,0 +1,31 @@
+package grains
+
+// BadIndexError is returned when an invalid
+// board index is used
+type BadIndexError struct{}
+func (e BadIndexError) Error() string {
+	return "Index must be between 1 and 64"
+}
+
+// Square takes an index and returns the
+// total square of grains
+func Square(index int) (uint64, error) {
+	// If the index is out of range
+	// return an error
+	if index <= 0 || index >= 65 {
+		return 0, BadIndexError{}
+	}
+	// shifting a bit one to the
+	// left squares the number
+	// Binary => Decimal
+	// 1 => 1
+	// 10 => 2
+	// 100 => 4
+	// 1000 => 8
+	return 1 << (index - 1), nil
+}
+
+// Total returns the total number of grains
+func Total() uint64 {
+	return 18446744073709551615
+}
