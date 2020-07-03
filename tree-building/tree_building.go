717a5c92aab8b941c6a90c6cf9dac28f904f3c26
David McAlpine <dave@mcalpine.io>
2020-07-02 20:17:46 -0400

    Tree Building: minor speed improvments

--- tree-building/tree_building.go
+++ tree-building/tree_building.go
@@ -17,13 +17,6 @@ type Node struct {
 	Children []*Node // Nodes which belong to this one
 }
 
-// SortRecord implements the Sort interface
-type SortRecord []Record
-
-func (r SortRecord) Len() int           { return len(r) }
-func (r SortRecord) Swap(i, j int)      { r[i], r[j] = r[j], r[i] }
-func (r SortRecord) Less(i, j int) bool { return r[i].ID < r[j].ID }
-
 // Build creates a tree from a slice of Records
 // The root node is returned, along with a
 // possible error
@@ -35,18 +28,16 @@ func Build(records []Record) (*Node, error) {
 	}
 	// sorting makes it easier to work with
 	// the data
-	sort.Sort(SortRecord(records))
+	sort.SliceStable(records, func(i, j int) bool {
+		return records[i].ID < records[j].ID
+	})
 
-	rootRecord := records[0]
 	// root record must be ID:0, Parent:0
 	// becase Root and Parent are ints, not
 	// setting either of these fields defaults
 	// them to 0
-	if rootRecord.Parent != 0 {
-		return nil, errors.New("Root nood cannot have a Parent")
-	}
-	if rootRecord.ID != 0 {
-		return nil, errors.New("Root node must have ID: 0")
+	if records[0] != (Record{}) {
+		return nil, errors.New("Root must be 0,0")
 	}
 
 	// nodeMap will keep allow us to access nodes
@@ -58,8 +49,7 @@ func Build(records []Record) (*Node, error) {
 	// insert our root node before iterating.
 	// root node is the only node that has the
 	// same ID and Parent {0,0}
-	rootNode := Node{rootRecord.ID, nil}
-	nodeMap[0] = &rootNode
+	nodeMap[0] = &(Node{records[0].ID, nil})
 
 	// loop over all subsequent records
 	for i := 1; i < len(records); i++ {
@@ -68,19 +58,11 @@ func Build(records []Record) (*Node, error) {
 
 		// do some error checking
 		switch {
-		case nodeMap[record.ID] != nil:
-			// duplicate check
-			return nil, errors.New(
-				"Node already exists, duplicates not allowed")
 		case record.ID != i:
 			// continuity check
 			return nil, errors.New(
 				"Node ID's are not sequential")
-		case record.ID == record.Parent:
-			// cycle directly check
-			return nil, errors.New(
-				"Node ID is same as Node Parent")
-		case nodeMap[record.Parent] == nil:
+		case record.Parent >= i:
 			// existing parent check
 			return nil, errors.New(
 				"Node's Parent does not exist")
@@ -91,7 +73,8 @@ func Build(records []Record) (*Node, error) {
 		// and add it to the children of the
 		// parent. this is the part which
 		// actually creates our tree structure
-		nodeMap[record.Parent].Children = append(nodeMap[record.Parent].Children, &node)
+		nodeMap[record.Parent].Children =
+			append(nodeMap[record.Parent].Children, &node)
 	}
-	return &rootNode, nil
+	return nodeMap[0], nil
 }
