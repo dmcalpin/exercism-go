7ee1322356b083fa21a0b9e00caeceb7b3aa0f19
David McAlpine <dave@mcalpine.io>
2020-07-03 21:11:48 -0400

    Tree Building: Cut time in half, added comments

--- tree-building/tree_building.go
+++ tree-building/tree_building.go
@@ -1,9 +1,6 @@
 package tree
 
-import (
-	"errors"
-	"sort"
-)
+import "errors"
 
 // Record is a database record
 type Record struct {
@@ -21,60 +18,70 @@ type Node struct {
 // The root node is returned, along with a
 // possible error
 func Build(records []Record) (*Node, error) {
-	// return nil instead of empty node if
-	// there are no records
-	if len(records) == 0 {
+	numRecords := len(records)
+	if numRecords == 0 {
 		return nil, nil
 	}
-	// sorting makes it easier to work with
-	// the data
-	sort.SliceStable(records, func(i, j int) bool {
-		return records[i].ID < records[j].ID
-	})
 
-	// root record must be ID:0, Parent:0
-	// becase Root and Parent are ints, not
-	// setting either of these fields defaults
-	// them to 0
-	if records[0] != (Record{}) {
-		return nil, errors.New("Root must be 0,0")
-	}
-
-	// nodeMap will keep allow us to access nodes
-	// by their ID so we don't have to iterate
-	// over the existing tree structure to find
-	// the parent
-	nodeMap := make(map[int]*Node)
-
-	// insert our root node before iterating.
-	// root node is the only node that has the
-	// same ID and Parent {0,0}
-	nodeMap[0] = &(Node{records[0].ID, nil})
-
-	// loop over all subsequent records
-	for i := 1; i < len(records); i++ {
-		record := records[i]
-		node := Node{record.ID, nil}
-
-		// do some error checking
-		switch {
-		case record.ID != i:
-			// continuity check
-			return nil, errors.New(
-				"Node ID's are not sequential")
-		case record.Parent >= i:
-			// existing parent check
-			return nil, errors.New(
-				"Node's Parent does not exist")
+	// nodeLocs is a sorted slice of nodes
+	// this is faster than sort.Sort but we
+	// take a hit on memory
+	nodeLocs := make([]*Node, numRecords)
+	for _, record := range records {
+		// error checking
+		if record.ID >= numRecords {
+			return nil, errors.New("Out of bounds")
+		}
+		if nodeLocs[record.ID] != nil {
+			return nil, errors.New("Duplicate Node")
 		}
 
-		// if all is good, add the node to our map
-		nodeMap[record.ID] = &node
-		// and add it to the children of the
-		// parent. this is the part which
-		// actually creates our tree structure
-		nodeMap[record.Parent].Children =
-			append(nodeMap[record.Parent].Children, &node)
+		// insert the node at it's proper location
+		nodeLocs[record.ID] = &Node{record.ID, nil}
 	}
-	return nodeMap[0], nil
+
+	for _, record := range records {
+		parentNode := nodeLocs[record.Parent]
+
+		// error checking
+		if parentNode.ID > record.ID {
+			return nil, errors.New("Bad parent ID")
+		}
+		if record.ID == record.Parent {
+			if record.ID == 0 {
+				continue
+			}
+			return nil, errors.New("Bad parent ID")
+		}
+
+		// if we already have children in the slice
+		// find out where to insert the new Node
+		// while maintaining order. This is faster
+		// than calling sort.Sort after simply
+		// appending the child
+		if len(parentNode.Children) > 0 {
+			var i = 0
+			for _, child := range parentNode.Children {
+				if record.ID < child.ID {
+					break
+				}
+				i++
+			}
+
+			// increases the length of the slice
+			parentNode.Children = append(parentNode.Children, nil)
+			// shift "half" of the slice over by one
+			copy(parentNode.Children[i+1:], parentNode.Children[i:])
+			// insert the new node in the "middle"
+			parentNode.Children[i] = nodeLocs[record.ID]
+		} else {
+			// if this is the first child just create the
+			// node slice
+			parentNode.Children = []*Node{nodeLocs[record.ID]}
+		}
+
+	}
+
+	// return the root node
+	return nodeLocs[0], nil
 }
