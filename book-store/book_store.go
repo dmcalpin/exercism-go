49a1897d5bd7c1cb3a3145fdd8bdaf723fba0878
Dave McAlpine <dave@mcalpine.io>
2021-03-31 14:23:56 -0400

    Book store - all but last test passing

--- book-store/book_store.go
+++ book-store/book_store.go
@@ -19,51 +19,53 @@ func Cost(basket []int) int {
 	sort.Ints(basket)
 
 	l := len(basket)
-
+	numUnique := countUnique(basket)
 	discount := countDiscount[1]
+
 	if allUnique(basket) {
-		discount = countDiscount[len(basket)]
+		discount = countDiscount[numUnique]
 	}
+
 	if l >= 5 {
 		lenFloat := float64(l)
 
-		// 5-based discount
-		num5discounts := float64(l / 5)
-		remainder := float64(l % 5)
+		// 4-based discount greedy (4 + remainder)
+		if numUnique >= 4 {
+			num4discounts := float64(l / 4)
+			remainder := float64(l % 4)
 
-		disc5 := countDiscount[5] * (5.0 * num5discounts / lenFloat)
-		remainderDisc := countDiscount[int(remainder)] * (remainder / lenFloat)
-		discount = disc5 + remainderDisc
+			disc4 := countDiscount[4] * (4.0 * num4discounts / lenFloat)
+			remainderDisc := countDiscount[int(remainder)] * (remainder / lenFloat)
+			discount = disc4 + remainderDisc
 
-		// 4-based discount
-		num4discounts := float64(l / 4)
-		remainder = float64(l % 4)
-
-		disc4 := countDiscount[4] * (4.0 * num4discounts / lenFloat)
-		remainderDisc = countDiscount[int(remainder)] * (remainder / lenFloat)
-		discount4 := disc4 + remainderDisc
-
-		if discount4 < discount {
-			discount = discount4
 		}
-	}
 
-	if l%6 == 0 {
-		disc4 := countDiscount[4] * (4.0 / 6.0)
-		disc2 := countDiscount[2] * (2.0 / 6.0)
-		discount = disc4 + disc2
-	} else if l%4 == 0 {
-		discount = countDiscount[4]
+		if numUnique == 5 { // 5-based discount greedy (5 + remainder)
+			num5discounts := float64(l / 5)
+			remainder := float64(l % 5)
+
+			disc5 := countDiscount[5] * (5.0 * num5discounts / lenFloat)
+			remainderDisc := countDiscount[int(remainder)] * (remainder / lenFloat)
+			discount5 := disc5 + remainderDisc
+
+			if discount5 < discount {
+				discount = discount5
+			}
+		}
+
 	}
 
 	return int(math.Round((discount*float64(l)*float64(bookCost))*100) / 100)
 }
 
 func allUnique(basket []int) bool {
-	for i := 1; i < len(basket); i++ {
-		if basket[i] == basket[i-1] {
-			return false
-		}
+	return countUnique(basket) == len(basket)
+}
+
+func countUnique(basket []int) int {
+	seen := map[int]bool{}
+	for i := 0; i < len(basket); i++ {
+		seen[basket[i]] = true
 	}
-	return true
+	return len(seen)
 }
