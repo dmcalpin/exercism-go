0f3b65cf0ccf0ff610ae88489395e7f116da7035
Dave McAlpine <dave@mcalpine.io>
2021-04-01 19:35:44 -0400

    Book store - all tests passing

--- book-store/book_store.go
+++ book-store/book_store.go
@@ -24,11 +24,16 @@ func Cost(basket []int) int {
 	if allUnique(basket) {
 		discount = countDiscount[l]
 	} else if l >= 5 {
-		for i := 3; i < 6; i++ {
-			groupedByI := groupUnique(basket, i)
+		var groups []grouping
+		for i := 0; i < 2; i++ {
+			if i == 0 {
+				groups = broadGrouping(basket)
+			} else {
+				groups = deepGrouping(basket)
+			}
 
 			tmpDiscount := 0.0
-			for _, group := range groupedByI {
+			for _, group := range groups {
 				lenGroup := len(group)
 				tmpDiscount += countDiscount[lenGroup] * float64(lenGroup) / float64(l)
 			}
@@ -74,36 +79,42 @@ func orderByMode(basket []int) []int {
 	return orderedBasket
 }
 
-func groupUnique(basket []int, targetCount int) [][]int {
-	lastNum := -1
-	groupIndex := 0
+type grouping map[int]bool
 
-	groups := [][]int{
-		{},
+func deepGrouping(basket []int) []grouping {
+	groups := []grouping{}
+
+	for _, v := range basket {
+		added := false
+		for _, g := range groups {
+			if !g[v] {
+				g[v] = true
+				added = true
+				break
+			}
+		}
+
+		if !added {
+			groups = append(groups, grouping{
+				v: true,
+			})
+		}
+
+	}
+	return groups
+}
+
+func broadGrouping(basket []int) []grouping {
+	groups := []grouping{}
+	numGroups := int(math.Floor(math.Sqrt((float64(len(basket))))))
+
+	for i := 0; i < numGroups; i++ {
+		groups = append(groups, grouping{})
 	}
 
-	baseGroupIndex := 0
-	for i := 0; i < len(basket); i++ {
-		groupIndex = baseGroupIndex
-
-		if basket[i] == lastNum {
-			groupIndex++
-			if groupIndex == len(groups) {
-				groups = append(groups, []int{})
-			}
-		}
-
-		groups[groupIndex] = append(groups[groupIndex], basket[i])
-		lastNum = basket[i]
-
-		if len(groups[groupIndex])%targetCount == 0 && i != len(basket)-1 {
-			baseGroupIndex++
-			if len(groups) == baseGroupIndex {
-				groups = append(groups, []int{})
-			}
-			lastNum = -1
-		}
-
+	for i, v := range basket {
+		groupIndex := i % numGroups
+		groups[groupIndex][v] = true
 	}
 	return groups
 }
