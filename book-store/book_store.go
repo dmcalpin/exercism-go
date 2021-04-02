45ff4bc7cdcaa05e87807c42755ee317d2257d5f
Dave McAlpine <dave@mcalpine.io>
2021-04-02 19:18:50 -0400

    Book store - performance refactor

--- book-store/book_store.go
+++ book-store/book_store.go
@@ -1,9 +1,5 @@
 package bookstore
 
-import (
-	"sort"
-)
-
 // The cost of single book
 // in cents
 const bookCost = 800
@@ -23,67 +19,50 @@ var countDiscount = map[int]float64{
 // discount for a given basket of
 // books
 func Cost(basket []int) int {
-	// this makes grouping
-	// easier/possible
-	sort.Ints(basket)
 	basketCount := len(basket)
+	baseBasketCost := bookCost * basketCount
 
-	baseBookCost := bookCost * basketCount
 	discount := 0.0
 	altDiscount := 0.0
 
-	grouping := groupBooks(basket)
-	discounts := getDiscounts(grouping)
-
-	for _, d := range discounts {
-		perBookDiscount := countDiscount[d]
-		discount += float64(d) * perBookDiscount / float64(basketCount)
-
-		if discounts[0] >= 4 && basketCount%4 == 0 {
-			perBookDiscount = countDiscount[4]
-			altDiscount += float64(d) * perBookDiscount / float64(basketCount)
+	// count each type of book
+	bookCounts := make(map[int]int, 5)
+	largestGroup := 0
+	for _, v := range basket {
+		bookCounts[v]++
+		if bookCounts[v] > bookCounts[largestGroup] {
+			largestGroup = v
 		}
-
 	}
+
+	// create slices of unique books
+	// 1,1,2,2,3 => 1,2,3 and 1,2
+	max := bookCounts[largestGroup]
+	bookSets := make([]int, max)
+	for i, _ := range bookSets {
+		for k, _ := range bookCounts {
+			if bookCounts[k] > 0 {
+				bookSets[i]++
+				bookCounts[k]--
+			}
+		}
+	}
+
+	// apply a discount either by num of books per set
+	// or try an alternet discount using a grouping of 4
+	for _, d := range bookSets {
+		discountPart := float64(d) / float64(basketCount)
+		discount += countDiscount[d] * discountPart
+
+		// covers 5 & 3 vs 4 & 4 comparison
+		if bookSets[0] >= 4 && basketCount%4 == 0 {
+			altDiscount += countDiscount[4] * discountPart
+		}
+	}
+
 	if altDiscount != 0 && altDiscount < discount {
 		discount = altDiscount
 	}
 
-	return int(discount * float64(baseBookCost))
-}
-
-func groupBooks(basket []int) map[int]int {
-	grouping := make(map[int]int)
-	for _, v := range basket {
-		grouping[v]++
-	}
-	return grouping
-}
-
-func getDiscounts(grouping map[int]int) []int {
-	groupCount := largestCount(grouping)
-	discounts := make([]int, groupCount)
-	for i := 0; i < groupCount; i++ {
-		for k, _ := range grouping {
-			if grouping[k] > 0 {
-				discounts[i]++
-				grouping[k]--
-			}
-		}
-	}
-	return discounts
-}
-
-func numUnique(basket []int) int {
-	return len(groupBooks(basket))
-}
-
-func largestCount(grouping map[int]int) int {
-	max := 0
-	for _, v := range grouping {
-		if v > max {
-			max = v
-		}
-	}
-	return max
+	return int(discount * float64(baseBasketCost))
 }
