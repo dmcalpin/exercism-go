2c57adc3dca7b72ec663ac1bbdf46a7162b22112
Dave McAlpine <dave@mcalpine.io>
2021-04-05 11:26:09 -0400

    Diffie-Hellman - fixed bug with declaring package-level big0

--- diffie-hellman/diffie_hellman.go
+++ diffie-hellman/diffie_hellman.go
@@ -6,20 +6,13 @@ import (
 	"math/big"
 )
 
-var (
-	// big0 is used at the beginning
-	// of each function call so that
-	// the passed-in big.Int is not
-	// modified
-	big0 = big.NewInt(0)
-	big2 = big.NewInt(2)
-)
+var big2 = big.NewInt(2)
 
 func PrivateKey(p *big.Int) *big.Int {
 	// Creates a key > 1 && < p
 	// 3 is subtracted because  the min is 2, and the
 	// max is p - 1, so 3 in total
-	a, err := rand.Int(rand.Reader, big0.Sub(p, big2))
+	a, err := rand.Int(rand.Reader, big.NewInt(0).Sub(p, big2))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -30,12 +23,12 @@ func PrivateKey(p *big.Int) *big.Int {
 
 func PublicKey(private, p *big.Int, g int64) *big.Int {
 	// A = g**a mod p
-	return big0.Exp(big.NewInt(g), private, p)
+	return big.NewInt(0).Exp(big.NewInt(g), private, p)
 }
 
 func SecretKey(private1, public2, p *big.Int) *big.Int {
 	// s = B**a mod p
-	return big0.Exp(public2, private1, p)
+	return big.NewInt(0).Exp(public2, private1, p)
 }
 
 func NewPair(p *big.Int, g int64) (private, public *big.Int) {
