642c0b63b96946ada212fe11a9b9ef13572411df
Dave McAlpine <dave@mcalpine.io>
2021-10-28 00:12:10 -0400

    Refactorted to be a bit more OO and removed regex
    The code is slightly longer now, but I think that it's more
    maintainable. Each private function is a bit smaller and easy to
    understand. Because of this refactoring I was able to split apart the
    matching logic and assign the appropriate matcher when a NewGrepper is
    called.
    
    The second largest update is getting rid of the regexp. Although very
    flexible. It took about half a second to MakeCompile, which accounted
    for about a third of the total processing time of Search. Using the
    strings package plus preselecting how to match resulted in an almost 50%
    decrease in processing time.
    
    Before:
    BenchmarkSearch-8   	    1189	    859111 ns/op	  332009 B/op	    1879 allocs/op
    
    After:
    BenchmarkSearch-8   	    2467	    496737 ns/op	  228050 B/op	     805 allocs/op

--- grep/grep.go
+++ grep/grep.go
@@ -2,81 +2,155 @@ package grep
 
 import (
 	"bufio"
+	"errors"
 	"log"
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
-func Search(pattern string, flags []string, files []string) []string {
-	matches := []string{}
+type Grep struct {
+	Pattern             string
+	SearchStr           string
+	FlagLineNo          bool
+	FlagFileName        bool
+	FlagInverse         bool
+	FlagCaseInsensitive bool
+	FlagMatchEntireLine bool
+	Filename            string
+	Regex               *regexp.Regexp
+	NumFiles            int
+	Matches             []string
+	CurrLine            int
+	lastText            string
+	matchString         func(string) bool
+}
 
-	flagLineNo := false
-	flagFileName := false
-	flagInverse := false
+func Search(pattern string, flags []string, files []string) []string {
+	config, err := NewGrepper(pattern, flags)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	config.NumFiles = len(files)
+
+	for _, filename := range files {
+		config.ScanFile(filename)
+	}
+
+	return config.Matches
+}
+
+func NewGrepper(
+	pattern string,
+	flags []string,
+) (*Grep, error) {
+	config := &Grep{
+		Pattern:   pattern,
+		SearchStr: pattern,
+		Matches:   []string{},
+	}
+
 	for _, flag := range flags {
 		switch flag {
 		case "-n":
-			flagLineNo = true
+			config.FlagLineNo = true
 		case "-i": // case insensitive
-			pattern = "(?i)" + pattern
+			config.FlagCaseInsensitive = true
 		case "-l":
-			flagFileName = true
+			config.FlagFileName = true
 		case "-x": // match entire line
-			pattern = "^" + pattern + "$"
+			config.FlagMatchEntireLine = true
 		case "-v":
-			flagInverse = true
+			config.FlagInverse = true
 		default:
-			log.Fatal("Error: Unknown flag", flag)
-			return nil
+			return nil, errors.New("Unknown flag: " + flag)
 		}
 	}
 
-	re := regexp.MustCompile(pattern)
-
-	numFiles := len(files)
-	for _, fileName := range files {
-		file, err := os.Open(fileName)
-		if err != nil {
-			log.Fatal(err)
+	if config.FlagMatchEntireLine {
+		if config.FlagCaseInsensitive {
+			config.matchString = config.insensitiveFullMatch
+		} else {
+			config.matchString = config.exactMatch
 		}
-		defer file.Close()
-
-		scanner := bufio.NewScanner(file)
-		line := 1
-		lastText := ""
-		for scanner.Scan() {
-			text := scanner.Text()
-			isMatch := re.MatchString(text)
-
-			// flagInverse causes isMatch to do
-			// do the opposite.
-			if flagInverse == isMatch {
-				line++
-				continue
-			}
-
-			if flagFileName {
-				// only add the file name once
-				if fileName == lastText {
-					continue
-				}
-				text = fileName
-			} else {
-				if flagLineNo {
-					text = strconv.Itoa(line) + ":" + text
-				}
-				if numFiles > 1 {
-					text = fileName + ":" + text
-				}
-			}
-
-			lastText = text
-			matches = append(matches, text)
-
-			line++
+	} else {
+		if config.FlagCaseInsensitive {
+			config.matchString = config.insensitivePartialMatch
+		} else {
+			config.matchString = config.partialMatch
 		}
 	}
 
-	return matches
+	return config, nil
+}
+
+func (config *Grep) ScanFile(filename string) {
+	config.Filename = filename
+	file, err := os.Open(config.Filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	config.CurrLine = 1
+	config.lastText = ""
+	for scanner.Scan() {
+		text := scanner.Text()
+
+		isMatch := config.matchString(text)
+
+		// flagInverse causes isMatch to do
+		// do the opposite.
+		if config.FlagInverse == isMatch {
+			config.CurrLine++
+			continue
+		}
+
+		text = config.decorateText(text)
+		if text == "" {
+			continue
+		}
+
+		config.lastText = text
+		config.Matches = append(config.Matches, text)
+
+		config.CurrLine++
+	}
+}
+
+func (g *Grep) decorateText(text string) string {
+	if g.FlagFileName {
+		// only add the file name once
+		if g.Filename != g.lastText {
+			text = g.Filename
+			return text
+		}
+		return ""
+	}
+	if g.FlagLineNo {
+		text = strconv.Itoa(g.CurrLine) + ":" + text
+	}
+	if g.NumFiles > 1 {
+		text = g.Filename + ":" + text
+	}
+	return text
+}
+
+func (g *Grep) insensitiveFullMatch(text string) bool {
+	return strings.EqualFold(text, g.SearchStr)
+}
+
+func (g *Grep) exactMatch(text string) bool {
+	return text == g.SearchStr
+}
+
+func (g *Grep) insensitivePartialMatch(text string) bool {
+	return strings.Contains(strings.ToLower(text), strings.ToLower(g.SearchStr))
+}
+
+func (g *Grep) partialMatch(text string) bool {
+	return strings.Contains(text, g.SearchStr)
 }
